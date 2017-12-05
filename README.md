222d0e194659e642007101a8fd9e80932f58e38d
Jay Song <jsong022@ucr.edu>
2017-12-05 01:27:35 -0800

    add new Activity by entering them into text input

--- README.md
+++ README.md
@@ -4,13 +4,15 @@
 A simple to-do list as practice for using jQuery.
 
 Goal:
-- [ ] enter new activity to the list by entering it into the text input
+- [x] enter new activity to the list by entering it into the text input
 - [x] delete listed activities by clicking on the \[X]'s
 - [x] strike-through "completed" activity by clicking on them (to remain on list)
 
 ###To-Do List 
 ######(for the To-Do List. ha ha. good joke.)
-- [ ] Add intended functionality
+- [x] Add intended functionality
+- *extra functionality*
+	- [x] Modify text input to prevent code injection
 - [ ] Make it pretty
 - [ ] \(Optional) Add stuff to to-do list if I think of more stuff to add 
 
--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,15 +1,46 @@
 $("body").addClass("check");
 
 //Denote completed activity by clicking on them
-$("li").on("click", function(){
+//Listener is on <ul> so future <li> added
+//can also have the desired action on click
+//
+//the Listner on <ul> listens for a "click" on
+//any <li> within it (current or future)
+$("ul").on("click", "li", function(){
+	//give the classes "activiy" and "delete" in this <li>
+	//a "completed" class for CSS styling
 	$(".activity", this).toggleClass("completed");
 	$(".delete", this).toggleClass("completed");
 });
 
 //Delete Activities by clicking on the [X]'s
-$("span.delete").on("click", function(event){
+//
+//the Listner on <ul> listens for a "click" on
+//the <span> with class "delete" within it
+$("ul").on("click", "span.delete", function(event){
+	//the parent <li> fades out 
 	$(this).parent().fadeOut(750, function(){
+		//delete the HTML code after fading out
 		$(this).remove()
 	});
+	//don't propagate the click to the <li> or its parents
 	event.stopPropagation();
+});
+
+//Add new Activity by entering them into input text
+$("input").on("keypress", function(event){
+	// on 'Enter' keypress
+	if (event.which === 13){
+		//grab the text input's value
+		var newActivity = $(this).val();
+		//replace '<' and '>' for code injection prevention
+		newActivity = newActivity.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+		//create the HTML code for the new Activity
+		var newHTML = "<li><span class=\"delete\">[X] </span><span class=\"activity\">"
+		newHTML += newActivity + "</span></li>";
+		//reset the input field
+		$(this).val("");
+		//add it to the to-do list
+		$("ul").append(newHTML);
+	}
 });
